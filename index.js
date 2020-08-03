a12742dd0b3bab9c5d03a13761f8f385185de809
treekimm <31647061+treekimm@users.noreply.github.com>
2020-08-03 16:20:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like test06!');
+    res.send('I like test07!');
 });
 
 app.get('/start', function(req, res){
